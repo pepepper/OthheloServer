a2b868980424a7e54b9089816da101cd85f10c80
pepepper <hollyholly2014@outlook.jp>
2019-03-27 04:51:19 +0900

    openssled

--- game.cpp
+++ game.cpp
@@ -4,7 +4,7 @@
 #include <sstream>
 #include <vector>
 
-int  write(BIO *con,std::string s){
+int  write(BIO *con, std::string s){
 	if(BIO_write(con, s.c_str(), s.length() + 1) <= 0){
 		std::cerr << "send error:" << errno << std::endl;
 		ERR_print_errors_fp(stderr);
@@ -13,7 +13,7 @@ int  write(BIO *con,std::string s){
 	return 0;
 }
 
-int read(BIO *con,char *data,int size){
+int read(BIO *con, char *data, int size){
 	if(BIO_read(con, data, size) <= 0){
 		std::cerr << "recv error:" << errno << std::endl;
 		ERR_print_errors_fp(stderr);
@@ -22,56 +22,26 @@ int read(BIO *con,char *data,int size){
 	return 0;
 }
 
-Game::Game(BIO *connection, int w, int h): host(connection), islocked(0), guest(-1), x(w), y(h), ended(0){
+Game::Game(BIO *connection, int w, int h): host(connection), islocked(0), x(w), y(h), ended(0){
 	std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
 	room = std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(start.time_since_epoch()).count());
 	std::string reply = "SUCCESS " + room;
-	if(write(host,reply)){
+	if(write(host, reply)){
 		BIO_free_all(host);
-		ended=1;
+		ended = 1;
 	}
 
 	thread = std::thread([this]{
 		char data[32] = {0};
 		std::string req;
 		while(1){
-			req.erase();
-
-				if(read(host, data, 32)){
-					write(guest, "CLOSED");
-					BIO_free_all(host);
-					ended=1;
-					break;
-				}
-				req = data;
-				if(write(guest, req) ){
-					if(send(host, "FAILED", 7, 0) == -1)std::cout << "send error:" << errno << std::endl;
-					std::cout << "send error:" << errno << std::endl;
-					socks[1].fd = 0;
-				}
-				if(send(host, "SUCCESS", 8, 0) == -1)std::cout << "send error:" << errno << std::endl;
-
-				if(recv(guest, data, 32, 0) == -1){
-					std::cout << "recv error:" << errno << std::endl;
-					send(host, "CLOSED", 7, 0);
-					socks[1].fd = 0;
-					break;
-				}
-				req = data;
-				if(send(host, req.c_str(), req.length() + 1, 0) == -1){
-					if(send(guest, "FAILED", 7, 0) == -1)std::cout << "send error:" << errno << std::endl;
-					std::cout << "send error:" << errno << std::endl;
-					socks[0].fd = 0;
-				}
-				if(send(guest, "SUCCESS", 8, 0) == -1)std::cout << "send error:" << errno << std::endl;
-			}
-
-			if(!req.compare("CLOSED")){
-				if(send(host, req.c_str(), req.length() + 1, 0) == -1)std::cout << "send error:" << errno << std::endl;
-				if(send(guest, req.c_str(), req.length() + 1, 0) == -1)std::cout << "send error:" << errno << std::endl;
+			if(read(host, data, 32)){//read host command
+				write(guest, "CLOSED");
+				ended = 1;
 				break;
-
-			if(req.find("SETPASSWORD") != -1){
+			}
+			req = data;
+			if(req.find("SETPASSWORD") != -1){//set password
 				std::vector<std::string> ret;
 				std::string temp;
 				std::stringstream ss{req};
@@ -81,7 +51,56 @@ Game::Game(BIO *connection, int w, int h): host(connection), islocked(0), guest(
 				islocked = 1;
 				password = ret[1];
 				std::string reply = "SUCCESS";
-				if(send(host, reply.c_str(), reply.length() + 1 + 1, 0) == -1)std::cout << "send error:" << errno << std::endl;
+				if(write(host, reply))ended = 1;
+				continue;
+			} else if(!req.compare("CLOSED")){//closed
+				write(host, req);
+				write(guest, req);
+				ended = 1;
+				break;
+			} else{
+				if(write(guest, req)){//send command to guest
+					write(host, "FAILED");
+					ended = 1;
+					break;
+				}
+				if(read(guest, data, 32)){//recv "SUCCESS" from guest
+					write(host, "CLOSED");
+					ended = 1;
+					break;
+				}
+				if(write(host, data)){//send "SUCCESS" to host
+					ended = 1;
+					break;
+				}
+			}
+
+			if(read(guest, data, 32)){//read guest command
+				write(host, "CLOSED");
+				ended = 1;
+				break;
+			}
+			req = data;
+			if(!req.compare("CLOSED")){//closed
+				write(host, req);
+				write(guest, req);
+				ended = 1;
+				break;
+			} else{
+				if(write(host, req)){//send command to host
+					write(guest, "FAILED");
+					ended = 1;
+					break;
+				}
+				if(read(host, data, 32)){//recv "SUCCESS" from host
+					write(guest, "CLOSED");
+					ended = 1;
+					break;
+				}
+				if(write(guest, data)){//send "SUCCESS" to guest
+					ended = 1;
+					break;
+				}
 			}
 		}
 		ended = 1;
@@ -91,47 +110,52 @@ Game::Game(BIO *connection, int w, int h): host(connection), islocked(0), guest(
 
 Game::~Game(){
 	ended = 1;
-	std::string req = "CLOSED";
-	if(send(host, req.c_str(), req.length() + 1, 0) == -1)std::cout << "send error:" << errno << std::endl;
-	if(send(guest, req.c_str(), req.length() + 1, 0) == -1)std::cout << "send error:" << errno << std::endl;
+	write(host, "CLOSED");
+	write(guest, "CLOSED");
 	thread.join();
-	close(host);
-	close(guest);
+	BIO_free_all(host);
+	BIO_free_all(guest);
 }
 
 void Game::readyGame(){
 	std::string reply = "READY";
-	if(send(host, reply.c_str(), reply.length() + 1, 0) == -1)std::cout << "send error:" << errno << std::endl;
-	if(send(guest, reply.c_str(), reply.length() + 1, 0) == -1)std::cout << "send error:" << errno << std::endl;
+	write(host, "READY");
+	write(guest, "READY");
 	return;
 }
 
 void Game::login(BIO *sock){
 	if(islocked == 0){
-		guest = *sock;
+		guest = sock;
 		std::string reply = "SUCCESS " + std::to_string(x) + " " + std::to_string(y);
-		if(send(guest, reply.c_str(), reply.length() + 1, 0) == -1)std::cout << "send error:" << errno << std::endl;
-		socks[1].fd = host;
-		socks[1].events = POLLIN | POLLERR;
+		if(write(guest, reply)){
+			BIO_free_all(guest);
+			return;
+		}
 		readyGame();
 	} else{
 		std::string reply = "FAILED";
-		if(send(*sock, reply.c_str(), reply.length() + 1, 0) == -1)std::cout << "send error:" << errno << std::endl;
+		if(write(sock, reply)){
+			BIO_free_all(sock);
+		}
 	}
 	return;
 }
 
 void Game::login(BIO *sock, std::string pass){
 	if(islocked == 1 && !password.compare(pass)){
-		guest = *sock;
+		guest = sock;
 		std::string reply = "SUCCESS " + std::to_string(x) + " " + std::to_string(y);
-		if(send(guest, reply.c_str(), reply.length() + 1, 0) == -1)std::cout << "send error:" << errno << std::endl;
-		socks[1].fd = host;
-		socks[1].events = POLLIN | POLLERR;
+		if(write(guest, reply)){
+			BIO_free_all(guest);
+			return;
+		}
 		readyGame();
 	} else{
 		std::string reply = "FAILED";
-		if(send(*sock, reply.c_str(), reply.length() + 1, 0) == -1)std::cout << "send error:" << errno << std::endl;
+		if(write(sock, reply)){
+			BIO_free_all(sock);
+		}
 	}
 	return;
 }
